a77a2c0c9e1a6223fd74d40ce7c21486296c8d3d
Cinfwat Probity <czprobity@gmail.com>
2015-03-09 02:06:45 +0100

    Added post handler for login with basic validation without DB and Session

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,32 @@ router.get('/', function(request, response, next) {
 });
 
 router.get('/login', function(request, response, next) {
-  response.send('Login page..');
+  response.render('login', { title: 'Express' });
 });
 
 router.post('/login', function(request, response, next) {
-  response.send('form request');
+  var username = request.body.username;
+  var password = request.body.password;
+  // var remember = request.sanitize('remember').toBoolean();
+  var remember = Boolean(request.body.remember);
+
+  request.checkBody('username', 'The Username field is required.').notEmpty();
+  if (username)
+    request.checkBody('username', 'The Username must be between 4 and 25 characters.').len(4, 25).optional();
+
+  request.checkBody('password', 'The Password field is required.').notEmpty();
+  if (password)
+    request.checkBody('password', 'The Password must be at least 4 characters.').len(4).optional();
+  // request.assert('password', 'The Password must not be more than 25 characters')
+
+  var errors = request.validationErrors();
+  if (errors) {
+    // console.log(errors);
+    response.render('login', { errors: errors, username: request.body.username });
+  } else {
+    response.send('form processing ...' + username+ '@' + password + ':' + remember);
+  }
+  // response.rend('form request');
 });
 
 router.get('/logout', function(request, response, next) {
