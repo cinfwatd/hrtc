5b5586e197ae8f3316423c649bff9db1029a98f8
Cinfwat Probity <czprobity@gmail.com>
2015-03-09 02:10:29 +0100

    Displaying just a string on the root (/) route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.send('welcome page...');
 });
 
 router.get('/dashboard', function(request, response, next) {
