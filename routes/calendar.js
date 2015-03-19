73df70d2d92866eeb5763c412c6eba9894b2faeb
Cinfwat Probity <czprobity@gmail.com>
2015-03-20 00:31:20 +0100

    Modify router.get to render the dashboard jade template instead of the initial/barbones return string

--- routes/calendar.js
+++ routes/calendar.js
@@ -2,7 +2,7 @@ var express = require('express'),
   router = express.Router();
 
 router.get('/', function(request, response, next) {
-  response.send('calendar');
+  response.render('calendar');
 });
 
 router.post('/save', function(request, response, next) {
