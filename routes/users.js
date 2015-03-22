21d4045c9b9e83b5b7f6a69f622326bd2bcdb0ca
Cinfwat Probity <czprobity@gmail.com>
2015-03-22 21:27:50 +0100

    Add clear menu items

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/settings', function(request, response, next) {
-  response.send('Settings page..........');
-});
+// router.get('/settings', function(request, response, next) {
+//   response.send('Settings page..........');
+// });
 
 router.get('/profile', function(request, response, next) {
   response.send('Profile Page.......');
