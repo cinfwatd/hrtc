49896387dbfb0514e64d156503834a2b8871b0a1
Cinfwat Probity <czprobity@gmail.com>
2015-03-25 11:53:34 +0100

    Add ajax profile picture upload
    Enable the user profile picture to be updated using ajax. this deletes the previous image from the uploads directory when upload is successful

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+  router = express.Router(),
+  fs = require('fs'),
+  path = require('path'),
+  User = require('../models/user');
 
 /* GET users listing. */
 router.get('/record', function(req, res, next) {
@@ -15,9 +18,38 @@ router.get('/profile', function(request, response, next) {
 });
 
 router.post('/profile-upload', function(request, response, next) {
-  console.log(request.files);
-  var url = '/uploads/' + request.files.avatar.name;
-  response.send({name:"yessss", status: "OK", url: url});
+  var id = request.session.userId;
+  var url = path.join('/uploads', request.files.avatar.name);
+  var deletePicture = function(url) {
+    var url = path.join(__dirname, '../public', url);
+    // console.log(url);
+    fs.unlink(url, function() {
+      return console.log("Error: User issues. file deleted.".red);
+    });
+  }
+  //find
+  User.findOne({_id: id}, function(error, user) {
+    if (error) {
+      deletePicture(url);
+      return response.send("Error uploading file.");
+    }
+
+    if (user) {
+      var oldPictureUrl = user.picture;
+      user.picture = url;
+      user.save(function(error, user) {
+        if (error){
+          deletePicture(url);
+          return response.send("Error uploading file.");
+        } else {
+          request.session.userPicture = url;
+          deletePicture(oldPictureUrl);
+          return response.send({status: "OK", url: url});
+        }
+      })
+    }
+  });
+
 });
 
 module.exports = router;
