d92f226b9a82b1d3b69559e1819fb97e5d26e80f
Cinfwat Probity <czprobity@gmail.com>
2015-03-27 15:10:56 +0100

    Add Appointments persistence to MongoDb
    And also checks for appointment between user and doctor

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   router = express.Router(),
-  User = require('../models/user');
+  User = require('../models/user'),
+  Appointment = require('../models/appointment');
 
 router.get('/', function(request, response, next) {
   response.render('doctors/list', {pageTitle: "Doctors"})
@@ -17,12 +18,43 @@ router.get('/:id', function(request, response, next) {
     }
 
     if (user) {
+      // this user = doctor
       var pageTitle = "Dr. " + user.name.full + "'s Profile";
-      return response.render('doctors/profile', {pageTitle: pageTitle, user: user});
+
+      var found = false;
+      var appoint = Appointment.findOne({
+        doctor: user.id,
+        patient: request.session.userId,
+        'chat.end': {$gt: Date.now()},
+        'status.granted': false,
+        'status.expired': false
+        });
+      if (appoint) found = true;
+      return response.render('doctors/profile', {pageTitle: pageTitle, user: user, doc: id, found: found});
     }
     request.flash('error', msg);
     return response.redirect('/doctors');
   });
+});
+
+router.post('/request', function(request, response, next) {
+  var start = request.body.start;
+  var end = request.body.end;
+  var message = request.body.msg;
+  var doc = request.body.doc;
+
+  var newRequest = Appointment();
+  newRequest.doctor = doc;
+  newRequest.patient = request.session.userId;
+  newRequest.message = message;
+  newRequest.chat.start = start;
+  newRequest.chat.end = end;
+  newRequest.save(function(error, appointment) {
+    if (error) return response.status(500).senk("Not Ok");
+    else return response.status(200).send("OK");
+  });
+  // newRequest.
+  // return response.status(200).send("OK");
 });
 
 module.exports = router;
