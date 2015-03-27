0016292f21e67766926958ae9ebfc2dd03647ef0
Cinfwat Probity <czprobity@gmail.com>
2015-03-27 15:07:57 +0100

    Modify appointment mongoose model
    this now includes chat intended start and end period, chat status

--- models/appointment.js
+++ models/appointment.js
@@ -6,8 +6,20 @@ var AppointmentSchema = new Schema({
   dateCreated: {type: Date, default: Date.now},
   doctor: ObjectId,
   patient: ObjectId,
-  date: Date,
-
+  message: String,
+  chat: {
+    video: Boolean,
+    text: Boolean,
+    start: Date,
+    end: Date
+  },
+  status: {
+    granted: {type: Boolean, default: false},
+    expired: {type: Boolean, default: false},
+    read: {type: Boolean, default: false},
+    deleted: {type: Boolean, default: false}
+  }
 });
 
 var Appointment = mongoose.model('Appointment', AppointmentSchema);
+module.exports = Appointment;
