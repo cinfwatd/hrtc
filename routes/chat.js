b71ab119efcf9f525cefed2893bece938fadfb5f
Cinfwat Probity <czprobity@gmail.com>
2015-04-07 23:21:31 +0100

    Remove the text chatting route and menu item

--- routes/chat.js
+++ routes/chat.js
@@ -6,8 +6,8 @@ router.get('/video', function(request, response, next) {
   response.render('video', {pageTitle: "Video Conference"});
 });
 //Text
-router.get('/text', function(request, response, next) {
-  response.render('text', {pageTitle: "Text Chat"});
-});
+// router.get('/text', function(request, response, next) {
+//   response.render('text', {pageTitle: "Text Chat"});
+// });
 
 module.exports = router;
