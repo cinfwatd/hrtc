af38f0c4df29fd264e8a525fa8716bd36c89a761
Cinfwat Probity <czprobity@gmail.com>
2015-05-17 21:02:27 +0100

    Add the patients route (list and profile) and
    Use the jquery quicksearch plugin to filter through list of patients

--- routes/patients.js
+++ routes/patients.js
@@ -5,13 +5,32 @@ var express = require('express'),
   async = require('async');
 
 router.get('/', function(request, response, next) {
-  response.send("patients page")
+  // response.send("patients page")
+  User.find({
+    groups: 'Patient',
+    'doctors.id': request.session.userId
+  }, function(error, patients) {
+    response.render('patients/list', {pageTitle: "Patients", patients: patients})
+  });
 });
 
 router.get('/:id', function(request, response, next) {
   var id = request.params.id;
 
-  response.send("Patient - ", id);
+  User.findOne({
+    _id: id,
+    groups: 'Patient'
+  }, function(error, patient) {
+    var msg = "): This is embarrassing. We couldn't find that User. Please try again!";
+    if (patient) {
+      var pageTitle = patient.name.full + "'s Profile";
+
+      return response.render('patients/profile', {pageTitle: pageTitle, user: patient});
+    } else {
+      request.flash('error', msg);
+      return response.redirect('/patients');
+    }
+  });
 });
 
 module.exports = router;
