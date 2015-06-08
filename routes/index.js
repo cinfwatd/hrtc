cfe77d3d71fd84e1910b3053766b97946c05cb70
Cinfwat Probity <czprobity@gmail.com>
2015-06-08 19:54:58 +0100

    Removed the add admin script from the index.js route

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,21 @@ var User = require('../models/user');
 router.get('/', function(request, response, next) {
   // response.send('welcome page...');
   // response.redirect('auth/login');
-  var newUser = User();
-    newUser.username = 'admin';
-    newUser.password = newUser.generateHash('pass');
-    newUser.name.first = 'User';
-    newUser.name.last = 'Super';
-
-    newUser.email = 'superuser@mailinator.com';
-    newUser.groups.push('Admin');
-
-    newUser.save(function(error, user) {
-      if (error) return console.log('error creating user');
-      // return console.log('New User created.');
-      return response.redirect('auth/login');
-    });
-
+  // var newUser = User();
+  //   newUser.username = 'admin';
+  //   newUser.password = newUser.generateHash('pass');
+  //   newUser.name.first = 'User';
+  //   newUser.name.last = 'Super';
+  //
+  //   newUser.email = 'superuser@mailinator.com';
+  //   newUser.groups.push('Admin');
+  //
+  //   newUser.save(function(error, user) {
+  //     if (error) return console.log('error creating user');
+  //     // return console.log('New User created.');
+  //     return response.redirect('auth/login');
+  //   });
+  return response.redirect('auth/login');
 });
 
 module.exports = router;
