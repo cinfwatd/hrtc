4ca5daa151eff3dc18d06a9d110dbdc952c5c6de
Cinfwat Probity <czprobity@gmail.com>
2015-06-08 11:18:38 +0100

    Add the admin creation script to the index route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(request, response, next) {
   // response.send('welcome page...');
-  response.redirect('auth/login');
+  // response.redirect('auth/login');
+  var newUser = User();
+    newUser.username = 'admin';
+    newUser.password = newUser.generateHash('pass');
+    newUser.name.first = 'User';
+    newUser.name.last = 'Super';
+
+    newUser.email = 'superuser@mailinator.com';
+    newUser.groups.push('Admin');
+
+    newUser.save(function(error, user) {
+      if (error) return console.log('error creating user');
+      // return console.log('New User created.');
+      return response.redirect('auth/login');
+    });
+
 });
 
 module.exports = router;
